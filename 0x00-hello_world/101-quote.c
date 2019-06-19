25bdbfdcaeb58d2fd059d423a21af9d1ffff6ea6
Sebastian Galvis <jsgalvish@gmail.com>
2019-06-19 16:36:13 -0500

    0x00 - Advance

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<unistd.h>
+
+/**
+ * main - Void
+ *
+ * Return: Show text and Errors
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
