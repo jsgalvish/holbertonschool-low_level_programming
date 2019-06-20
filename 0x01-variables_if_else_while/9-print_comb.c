d523bd31ff12c6b6cb99329d379f3bd71c53a7b2
Sebastian Galvis <jsgalvish@gmail.com>
2019-06-20 12:31:24 -0500

    0x01 - Task 09

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - void
+ *
+ * Return: numbers with ,
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+
+		if (i != 58)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
