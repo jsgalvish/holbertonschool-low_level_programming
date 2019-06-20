6af261f5cb869a9878925f32f72f61437b00fff8
Sebastian Galvis <jsgalvish@gmail.com>
2019-06-20 12:02:28 -0500

    0x01 - Task 05

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - void
+ *
+ * Return: Numbers
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; ++i)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
