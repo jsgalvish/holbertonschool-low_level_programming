161d81e3765b595c0fe8048a73a19b9a4d9cc15c
Sebastian Galvis <jsgalvish@gmail.com>
2019-06-20 11:09:26 -0500

    0x01 - Task 02

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - void
+ *
+ * Return: Alphabet
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
