482b74ef721fa3afe3d7847eb6bef337bc87742d
Sebastian Galvis <juansgalvish@gmail.com>
2019-06-25 02:41:18 -0500

    0x02 - Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all of the numbers to 98
+ * @n: initial position
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%i", n);
+			if (n != 98)
+			{
+				printf(",");
+				printf(" ");
+			}
+			n++;
+		}
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%i", n);
+			if (n != 98)
+			{
+				printf(",");
+				printf(" ");
+			}
+			n--;
+		}
+		printf("\n");
+	}
+	else
+	{
+		printf("%i", n);
+		printf("\n");
+	}
+}
