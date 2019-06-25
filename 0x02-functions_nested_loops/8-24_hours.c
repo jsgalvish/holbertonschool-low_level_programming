4a686e448568d465c61272d21c7ecdf223414769
Sebastian Galvis <juansgalvish@gmail.com>
2019-06-25 02:15:41 -0500

    0x02 - Task 08

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - Prints a 24 h clock
+ *
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = '0'; i <= '2'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '5'; k++)
+			{
+				for (l = '0'; l <= '9'; l++)
+				{
+					if (i >= '2' && j >= '4')
+						break;
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
