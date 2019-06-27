d169b02f3592f1e3bf08b8be82462462d09cdbcb
Sebastian Galvis <jsgalvish@gmail.com>
2019-06-27 10:01:25 -0500

    0x04 - Task 00

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* _isupper - function to check is upercase
+* @c: number to check
+* Return: 1 if is uppercase
+*/
+
+int _isupper(int c)
+{
+	int i = 0;
+
+	if (c <= 56 || c >= 90)
+		i = 1;
+	return (i);
+}
