3712f28e0cdd810474475b1c4e05523a498432f8
Sebastian Galvis <jsgalvish@gmail.com>
2019-07-02 11:52:29 -0500

    0x05 - Task 00

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+* reset_to_98 - changes the value of a pointer
+*
+* @n: Pointer to change
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,7 @@
+#ifndef _holberton_h_
+#define _holberton_h_
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
