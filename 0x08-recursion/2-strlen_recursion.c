419bd754a3490c9b41138c3d7f814d2ef3955496
Sebastian Galvis <jsgalvish@gmail.com>
2019-07-10 14:50:01 -0500

    0x08 - Task 02

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* _strlen_recursion - count lenght
+* @s: string
+* Return: size;
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+
+	return (0);
+}
