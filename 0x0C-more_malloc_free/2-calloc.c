ab212cce9285fd46217dbc4b8e51a9ae4fd88a33
Sebastian Galvis <jsgalvish@gmail.com>
2019-07-18 21:52:25 -0500

    0x0C - Task 02

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * *_calloc - Fuction
+ * @nmemb: int
+ * @size: int
+ * Return: char
+**/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(size * nmemb);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < (size * nmemb); i++)
+		p[i] = '\0';
+
+	return ((void *)p);
+}
