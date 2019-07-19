5aa5be47fd80b4394cb8e5bbbdd4ebe8ccd2c8b9
Sebastian Galvis <jsgalvish@gmail.com>
2019-07-18 22:13:54 -0500

    0x0C - Task Advance

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * *_realloc - Fuction
+ * @ptr: pointer
+ * @old_size: int
+ * @new_size: int
+ * Return: void
+**/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *ret = 0;
+	char *write, *read;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size > 0 || ptr == 0)
+	{
+		ret = malloc(new_size);
+		if (ret == 0)
+			return (0);
+	}
+	if (new_size > 0 && ptr != 0)
+	{
+		write = ret;
+		read = ptr;
+		if (new_size < old_size)
+			old_size = new_size;
+		while (old_size)
+		{
+			old_size--;
+			*write++ = *read++;
+		}
+	}
+
+	free(ptr);
+	return (ret);
+}
