e0b17d406eff12a0ce73a7fa7fd9817a84ecf927
Sebastian Galvis <jsgalvish@gmail.com>
2019-07-22 14:08:34 -0500

    0x0E - Task 00

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG
+#define DOG
+/**
+* struct dog - struct with 3 arguments
+* @name: dog name
+* @age: dog age
+* @owner: owner of dog
+**/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
