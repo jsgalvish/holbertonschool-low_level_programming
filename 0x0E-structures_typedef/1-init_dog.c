d27a2e4e4a80fc65ab8733acd05af53d7adb3741
Sebastian Galvis <jsgalvish@gmail.com>
2019-07-22 14:22:00 -0500

    0x0E - Task 01

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+* init_dog - initialize structure dog
+* @d: structure dog
+* @name: dog name
+* @age: dog age
+* @owner: dog owner
+**/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
