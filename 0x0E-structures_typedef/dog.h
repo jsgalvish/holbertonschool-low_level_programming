9da93db944413917257ddf401d80a7343cb74d2d
Sebastian Galvis <jsgalvish@gmail.com>
2019-07-22 14:57:28 -0500

    0x0E - Task 03

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,13 @@
 * @age: dog age
 * @owner: owner of dog
 **/
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *n_dog(char *name, float age, char *owner);
 #endif
