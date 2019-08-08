8e83865ecb9c1a2240aa3b19f7f7d5723226c183
jsgalvish <jsgalvish@unal.edu.co>
2019-08-08 18:06:21 -0500

    0x14 - Task 00

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - convert binary to int
+ * @b: number to conver
+ * Return: int or 0 if error or NULL
+ **/
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int num;
+
+	num = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
+	}
+	return (num);
+}
