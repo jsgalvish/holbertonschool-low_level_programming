c7a1768e948b6919a4ceca7dddce396bc9ae82e0
jsgalvish <jsgalvish@unal.edu.co>
2019-08-08 18:42:16 -0500

    0x14 - Task 05

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - number of different bits between numbers numbers
+ * @n: first number
+ * @m: second number
+ * Return: number of bits to flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int diff, check;
+	unsigned int count, i;
+
+	count = 0;
+	check = 1;
+	diff = n ^ m;
+	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
+	{
+		if (check == (diff & check))
+			count++;
+		check <<= 1;
+	}
+	return (count);
+}
